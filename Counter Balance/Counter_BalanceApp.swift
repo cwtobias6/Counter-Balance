b5c7d45f078a86e311d183a0047b2ef8ae8681f1
Christian Tobias <cwtobias6@gmail.com>
2020-10-20 19:53:13 -0700

    Initial Commit

---
+++ Counter Balance/Counter_BalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Counter_BalanceApp.swift
+//  Counter Balance
+//
+//  Created by Christian Tobias on 10/20/20.
+//
+
+import SwiftUI
+
+@main
+struct Counter_BalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
